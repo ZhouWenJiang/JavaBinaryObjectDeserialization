6262f58c7fa0e83104e2caeb2977aab54f561312
zak <zak@fcuh.com>
2016-09-30 16:15:15 +0800

    首次提交

---
+++ src/main/java/com/zz/serialized/ExceptionContent.java
@@ -0,0 +1,42 @@
+/* Copyright (C) 2004-2007 Sami Koivu
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package com.zz.serialized;
+
+
+import java.util.List;
+
+import com.zz.util.ByteSerializer;
+
+
+public class ExceptionContent implements Content {
+
+  public Content exception;
+
+  public ExceptionContent( Content exception ) {
+    this.exception = exception;
+  }
+
+  public Content getException() {
+    return this.exception;
+  }
+
+  public void serialize( ByteSerializer serializer, List<Object> handles ) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
